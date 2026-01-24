0d26a254d5370cbff6c4e071c069c7b8fa41c5d8
Ali Deniz Yakar <denizyakar00@gmail.com>
2026-01-24 16:07:48 +0300

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Ali Deniz Yakar on 24.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
