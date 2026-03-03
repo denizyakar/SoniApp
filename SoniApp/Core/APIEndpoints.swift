f736f25e0e2d403a48953406fdec61cc9386004a
Ali Deniz Yakar <denizyakar00@gmail.com>
2026-03-03 18:16:19 +0300

    Update APIEndpoints.swift

--- SoniApp/Core/APIEndpoints.swift
+++ SoniApp/Core/APIEndpoints.swift
@@ -10,7 +10,7 @@ import Foundation
 
 enum APIEndpoints {
     
-    static let baseURL = "https://soni-app.xyz"
+    static let baseURL = "url"
     
     // MARK: - Auth
     static var login: URL {
